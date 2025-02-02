b1dda768110fd13983a8b0a876d5f4b42cc7f3e9
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2025-02-02 08:28:48 -0300

    fix comunicação

--- src/public/scripts.js
+++ src/public/scripts.js
@@ -1,30 +1,28 @@
 //redireciona para a tela de cadastro de usuario
 function abrirCadastro() {
-    window.location.href = "cadastro.html";
+  window.location.href = "cadastro.html";
 }
 
 //volta para a tela de login
 function voltarLogin() {
-    window.location.href = "index.html";
+  window.location.href = "index.html";
 }
 
 //simula o login e redireciona para a pagina de prospects
 function realizarLogin(event) {
-    event.preventDefault(); // evita o envio padrao do formulario
-    alert("Login efetuado com sucesso! (backend)");
-    window.location.href = "prospects.html"; // redireciona para a página de prospects
+  event.preventDefault(); // evita o envio padrao do formulario
+  alert("Login efetuado com sucesso! (backend)");
+  window.location.href = "prospects.html"; // redireciona para a página de prospects
 }
 
 //alerta para a tela de prospects
 function cadastrarProspect() {
-    alert("funcao de cadastro de prospect backend.");
+  alert("funcao de cadastro de prospect backend.");
 }
 
 //simula o cadastro de um usuario
 function cadastrarUsuario(event) {
-    event.preventDefault();
-    alert("Usuário cadastrado com sucesso! (backend)");
-    window.location.href = "index.html";
+  event.preventDefault();
+  alert("Usuário cadastrado com sucesso! (backend)");
+  window.location.href = "index.html";
 }
-
-);
